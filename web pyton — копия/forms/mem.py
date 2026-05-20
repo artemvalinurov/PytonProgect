54ee9a29bc6cc03506658b93ab8e212574d8f3c0
artemvalinurov <162029711+artemvalinurov@users.noreply.github.com>
2026-05-20 21:35:36 +0700

    Delete web pyton — копия directory

--- web pyton — копия/forms/mem.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-
-class MemForm(FlaskForm):
-    description = TextAreaField('Описание фотографии', validators=[DataRequired()])
-    photo = FileField('Загрузите файл с фотографией', validators=[DataRequired()])
-    submit = SubmitField('Добавить')
